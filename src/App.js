4ae19acd638ae28d893f4176613043b4716917f3
Utkarsh <utkarshrai13@gmail.com>
2024-08-21 22:18:08 +0530

    Added basic layout of home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import EditorPage from'./pages/EditorPage';
+
 
 function App() {
   return (
@@ -7,6 +10,10 @@ function App() {
           <BrowserRouter>
               <Routes>
                 <Route path = "/" element = {<Home/>}></Route>
+                <Route 
+                    path = "/editor/:roomId" 
+                    element = {<EditorPage/>}>
+                </Route>
               </Routes>
           
           
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home Page
+    <div className='homePageWrapper'>
+      <div className='formWrapper'>
+        <img src='/code-sync.png' alt='code-sync-logo'/>
+        <h4 className='mainLabel'>Paste Invitation ROOM ID</h4>
+        <div className='inputGroup'>
+            <input 
+            type='text' 
+            className='inputBox' 
+            placeholder='ROOM ID'
+            />
+            <input 
+            type='text' 
+            className='inputBox' 
+            placeholder='USERNAME'
+            />
+            <button className='btn joinBtn'>Join</button>
+            <span className='createInfo'>
+              If you don't have an invite then create &nbsp;
+              <a href='' className='createNewBtn'>New Room</a>
+            </span>
+        </div>
+      </div>
+      <footer className='footer'>
+        <h4>
+          Built with ❤️ by &nbsp; 
+          <a href='https://github.com/Ultimateutkarsh11'>Utkarsh Rai</a>
+        </h4>
+      </footer>
     </div>
   )
 }
