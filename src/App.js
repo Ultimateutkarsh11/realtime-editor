53e2328cca43624829522b97b98a095195853d51
Utkarsh <utkarshrai13@gmail.com>
2024-08-23 23:09:33 +0530

    Added styling to Home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
 import Home from './pages/Home';
 import EditorPage from'./pages/EditorPage';
 
@@ -7,6 +8,18 @@ import EditorPage from'./pages/EditorPage';
 function App() {
   return (
       <>
+      <div>
+        <Toaster
+          position = "top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: '#4aed88',
+              },
+            },
+          }}
+          ></Toaster>
+      </div>
           <BrowserRouter>
               <Routes>
                 <Route path = "/" element = {<Home/>}></Route>
@@ -15,13 +28,7 @@ function App() {
                     element = {<EditorPage/>}>
                 </Route>
               </Routes>
-          
-          
-          
-          
           </BrowserRouter>
-
-      
       </>
   );
 }
