257eef13b025532297b72bdd593a9ee31ae0df1b
Utkarsh <utkarshrai13@gmail.com>
2024-11-26 23:46:03 +0530

    Bug fixes + minor updates

---
+++ src/Actions.js
@@ -0,0 +1,13 @@
+// All events are listed here:
+// A constant object called ACTIONS is defined containing key-value pairs representing different event types.
+
+const ACTIONS = {
+  JOIN: 'join',
+  JOINED: 'joined',
+  DISCONNECTED: 'disconnected',
+  CODE_CHANGE: 'code-change',
+  SYNC_CODE: 'sync-code',
+  LEAVE: 'leave',
+};
+
+module.exports = ACTIONS;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,44 +1,82 @@
-import React, {useState} from 'react'
-import Client from '../compnonents/Client';
-import Editor from '../compnonents/Editor';
+import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
+import ACTIONS from "../Actions";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 
 const EditorPage = () => {
-  const [clients, setClients] = useState([
-    {socketId: 1, username: 'Rakesh'},
-    {socketId: 2, username: 'John Doe'},
-    {socketId: 3, username: 'Utkarsh'},
-  ]);
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for JOINED event:
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room!`);
+            console.log(`${username} joined`);
+          }
+        }
+      );
+    };
+
+    init();
+  }, []);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
 
   return (
-    <div className='mainWrap'>
-      <div className='aside'>
-        <div className='asideInner'>
-          <div className='logo'>
-            <img 
-            className= "logoImage" 
-            src= "/code-sync.png" 
-            alt= "logo"
-            />
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
           </div>
           <h3>Connected</h3>
           <div className="clientsList">
-            {
-              clients.map(client => (
-                <Client
-                  key = {client.socketId}
-                  username = {client.username}
-                />
-              ))}
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
           </div>
         </div>
-        <button className='btn copyBtn'>Copy ROOM ID</button>
-        <button className='btn leaveBtn'>Leave</button>
+        <button className="btn copyBtn">Copy ROOM ID</button>
+        <button className="btn leaveBtn">Leave</button>
       </div>
-      <div className='editorWrap'>
+      <div className="editorWrap">
         <Editor />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditorPage
+export default EditorPage;
